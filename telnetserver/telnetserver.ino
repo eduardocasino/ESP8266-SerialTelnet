cb3e1eaec9606367ed3550f7252c91c20c96ea47
ryaneroth <ryan.roth@sonos.com>
2024-10-02 16:54:06 -0600

    Initial commit for KIM-1

--- telnetserver/telnetserver.ino
+++ telnetserver/telnetserver.ino
@@ -1,48 +1,44 @@
-
-//PLEASE NOTE:
-//require ESP8266 >= 2.4.0 https://github.com/esp8266/Arduino/releases/tag/2.4.0-rc1
-//for the use of "setRxBufferSize" function
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h> 
 #include <Hash.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>
+
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
+
+WiFiServer server(23);
+WiFiClient serverClient;
+WiFiManager wifiManager;
+
+#define BAUD_RATE 9600
+
 const char* update_path = "/firmware";
 const char* update_username = "admin";
 const char* update_password = "admin";
-
-
-#define MAX_SRV_CLIENTS 1
-WiFiServer server(23);
-WiFiClient serverClient;
-
 int RESET_PIN = 0; // = GPIO0 on nodeMCU
-WiFiManager wifiManager;
 
 void setup()
 {
-	Serial.setRxBufferSize(1024); //require ESP8266 >= 2.4.0 https://github.com/esp8266/Arduino/releases/tag/2.4.0-rc1
-	Serial.begin(115200);
+  Serial.begin(BAUD_RATE, SERIAL_8N1);
+	Serial.setRxBufferSize(1024); // requires ESP8266 >= 2.4.0 https://github.com/esp8266/Arduino/releases/tag/2.4.0-rc1
 
-    delay(5000); //BOOT WAIT
-    pinMode(RESET_PIN, INPUT_PULLUP);
-    wifiManager.autoConnect("ESP8266");
+  delay(5000); // BOOT WAIT
+  pinMode(RESET_PIN, INPUT_PULLUP);
+  wifiManager.setHostname("KIM1");
+  wifiManager.autoConnect("KIM1");
 
 	server.begin();
 	server.setNoDelay(true);
 
-    httpUpdater.setup(&httpServer, update_path, update_username, update_password);
-    httpServer.begin();
+  httpUpdater.setup(&httpServer, update_path, update_username, update_password);
+  httpServer.begin();
 
-    WiFi.setSleepMode(WIFI_NONE_SLEEP); // disable WiFi sleep for more performance
+  WiFi.setSleepMode(WIFI_NONE_SLEEP); // disable WiFi sleep for more performance
 }
-
 
 void loop()
 {
@@ -58,28 +54,33 @@ void AcceptConnection()
 {
 	if (serverClient && serverClient.connected()) 
 		serverClient.stop();
-
 	serverClient = server.available();
-	serverClient.write("ESP8266 Connected!\n");
+  // IAC WILL SUPPRESS-GO-AHEAD
+  serverClient.write(255);
+  serverClient.write(253);
+  serverClient.write(3);
+  // I WILL ECHO
+  serverClient.write(255);
+  serverClient.write(251);
+  serverClient.write(1);
+	serverClient.write("Connected\n");
 }
 
 void ManageConnected()
 {
-    size_t rxlen = serverClient.available();
-    if (rxlen > 0)
+  size_t rxlen = serverClient.available();
+  if (rxlen > 0)
 	{
-		uint8_t sbuf[rxlen];
-		serverClient.readBytes(sbuf, rxlen);
-        Serial.write(sbuf, rxlen);
+		char sbuf[rxlen];
+		serverClient.read(sbuf, rxlen);
+    Serial.write(sbuf, rxlen);
 	}
-	
-    size_t txlen = Serial.available();
-    if (txlen > 0)
+  
+  size_t txlen = Serial.available();
+  if (txlen > 0)
 	{
-		uint8_t sbuf[txlen];
-		Serial.readBytes(sbuf, txlen);
-        serverClient.write(sbuf, txlen);
+		char sbuf[txlen];
+		Serial.read(sbuf, txlen);
+    serverClient.write(sbuf, txlen);
 	}
 }
-
-
